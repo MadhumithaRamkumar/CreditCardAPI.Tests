930d954372d6520e2a99506068dd74c1405375dd
Madhumitha <d.madhumitha@gmail.com>
2025-11-06 12:17:50 +1300

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,22 @@
+using CreditCardAPI.Services;
+namespace CreditCardAPI.Tests
+{
+    public class UnitTest1
+    {
+        private readonly ILuhnService _service = new LuhnService();
+        [Theory]
+
+        [InlineData("4242 4242 4242 4242", true)]
+        [InlineData("4242424242424242", true)]
+        [InlineData("4111111111111111", true)]
+        [InlineData("4242424242424241", false)]
+        [InlineData("", false)]
+
+        [InlineData("abcd-1234", false)]
+
+        public void Test1(string number, bool expected)
+        {
+            Assert.Equal(expected, _service.IsValid(number).IsValid);
+        }
+    }
+}

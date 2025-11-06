fcacf118244bc6d5f859521d554ba743c6afa7db
Madhumitha <d.madhumitha@gmail.com>
2025-11-06 20:14:48 +1300

    Addition of additional  scenarios

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -5,13 +5,14 @@ namespace CreditCardAPI.Tests
     {
         private readonly ILuhnService _service = new LuhnService();
         [Theory]
-
+        
+        [InlineData("5555555555554444", true)]
         [InlineData("4242 4242 4242 4242", true)]
         [InlineData("4242424242424242", true)]
         [InlineData("4111111111111111", true)]
         [InlineData("4242424242424241", false)]
         [InlineData("", false)]
-
+        [InlineData("****", false)]
         [InlineData("abcd-1234", false)]
 
         public void Test1(string number, bool expected)
